05607f50698b251fafa6d20a23797c16dfd56dec
PAULO HENRIQUE DE ARAUJO RODRIGUES <PAULO0855@DIGITALCOLLEGE.LOCAL>
2024-06-26 13:25:01 -0300

    ...

---
+++ calculodeSalarioFinal.js
@@ -0,0 +1,11 @@
+// beneficios sob tempo de empresa
+
+// menos de 1 ano ------ 0% sob o salario base
+// de 2 a 5 anos ------- 10% sob o salario base
+// de 5 a 10 anos ------- 15% sob o salario base
+// mais de 10 anos --------- 20% sob o salario base
+
+// benefício por dependente
+// 500,00 por dependente menor que 7 anos
+
+// VA ----------- R$ 
