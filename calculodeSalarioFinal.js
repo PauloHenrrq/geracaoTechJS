db099db3416788d7f2466a94f360f3884b7b46d0
PH <paulofera159@gmail.com>
2024-06-27 09:51:15 -0300

    Cálculo de Salário Final feito (Diferente da professora)

--- calculodeSalarioFinal.js
+++ calculodeSalarioFinal.js
@@ -7,19 +7,25 @@
 
 let nome = prompt('Qual seu nome?')
 let salario = Number(prompt(`${nome}, qual o seu salario?`))
-let tempo = (Number(prompt(`${nome}, há quanto tempo você está na empresa? \n 1 - "menos de 1 ano a 2 anos \n 2 - "2 a 5 anos" \n 3 - "5 a 10 anos" \n 4 - "mais de 10 anos"`)))
-let crianca = prompt(`${nome}, você possui filhos que possui idade inferior a 7 anos? (Sim/Não)`)
+if (isNaN(salario)) {
+    console.log('Precisa ser um número!')
+}
+else {
+    tempo = (Number(prompt(`${nome}, há quanto tempo você está na empresa? \n 1 - "menos de 1 ano a 2 anos \n 2 - "2 a 5 anos" \n 3 - "5 a 10 anos" \n 4 - "mais de 10 anos"`)))
+    crianca = prompt(`${nome}, você possui filhos que possui idade inferior a 7 anos? (Sim/Não)`)
+}
 
-VA = 1000.00
-VT = 200.00
+VA = 1000.00 // Vale Alimentação
+VT = 200.00 // Vale Transporte
+INSS = 1.09
+FGTS = 1.08
+
+
 
 crianca.toLowerCase()
 
 if (crianca !== 'sim' && crianca !== 'não') {
     console.log('Escreva "sim" ou "não"')
-}
-else if (salario == NaN) {
-    console.log('Precisa ser um número!')
 }
 else {
     switch (tempo) {
@@ -28,24 +34,29 @@ else {
                 qntFilhos = Number(prompt(`${nome}, quantos filhos com idade inferior a 7 anos você tem?`))
                 beneficioDep = qntFilhos * 500
                 salarioB = salario + beneficioDep
-                salarioF = salarioB / 1.17
-                
-                console.log(`Salário final (descontado INSS e FGTS): R$ ${salarioF.toFixed(0)}`)
-                console.log(`Vale Alimentação; R$ ${VA}`)
-                console.log(`Vale transporte: R$ ${VT}`)
-                salarioFB = salarioF + VA + VT
-                console.log(`Somatória do salário e benefícios: R$ ${salarioFB.toFixed(0)}`)
-                
+
+                console.log(`Nome: ${nome}`)
+                console.log(`Salário Base: ${salario}`)
+                console.log(`Bônus por tempo de serviço: 0`)
+                console.log(`Auxílio Creche: ${beneficioDep}`)
+                console.log(`Vale Transporte: ${VT}`)
+                console.log(`Vale Alimentação: ${VA}`)
+                console.log(`Desconto INSS: ${salario - INSS}`)
+                console.log(`Desconto FGTS: ${salario - FGTS}`)
+                console.log(`Total a receber: ${salario + beneficioDep + VT + VA - INSS - FGTS}`)
+
             } else {
-                salarioF = salario / 1.17
-                
-                console.log(`Salário final (descontado INSS e FGTS): R$ ${salarioF.toFixed(0)}`)
-                console.log(`Vale Alimentação; R$ ${VA}`)
-                console.log(`Vale transporte: R$ ${VT}`)
-                salarioFB = salarioF + VA + VT
-                console.log(`Somatória do salário e benefícios: R$ ${salarioFB.toFixed(0)}`)
+                console.log(`Nome: ${nome}`)
+                console.log(`Salário Base: ${salario}`)
+                console.log(`Bônus por tempo de serviço: 0`)
+                console.log(`Auxílio Creche: 0`)
+                console.log(`Vale Transporte: ${VT}`)
+                console.log(`Vale Alimentação: ${VA}`)
+                console.log(`Desconto INSS: ${salario - INSS}`)
+                console.log(`Desconto FGTS: ${salario - FGTS}`)
+                console.log(`Total a receber: ${salario + VT + VA - INSS - FGTS}`)
             }
-    
+
             break;
 
         case 2:
@@ -54,20 +65,94 @@ else {
                 beneficioDep = qntFilhos * 500
                 salarioB = salario + beneficioDep
 
-                
+                bonus = (salario * 1.10) - salario
 
-                salarioF = salarioB / 1.17
-                
-                console.log(`Salário final (descontado INSS e FGTS): R$ ${salarioF.toFixed(0)}`)
-                console.log(`Vale Alimentação; R$ ${VA}`)
-                console.log(`Vale transporte: R$ ${VT}`)
-                salarioFB = salarioF + VA + VT
-                console.log(`Somatória do salário e benefícios: R$ ${salarioFB.toFixed(0)}`)
-                
+                console.log(`Nome: ${nome}`)
+                console.log(`Salário Base: ${salario}`)
+                console.log(`Bônus por tempo de serviço: ${bonus}`)
+                console.log(`Auxílio Creche: ${beneficioDep}`)
+                console.log(`Vale Transporte: ${VT}`)
+                console.log(`Vale Alimentação: ${VA}`)
+                console.log(`Desconto INSS: ${salario - INSS}`)
+                console.log(`Desconto FGTS: ${salario - FGTS}`)
+                console.log(`Total a receber: ${salario + bonus + beneficioDep + VT + VA - INSS - FGTS}`)
+
             } else {
-                
+                console.log(`Nome: ${nome}`)
+                console.log(`Salário Base: ${salario}`)
+                console.log(`Bônus por tempo de serviço: ${bonus}`)
+                console.log(`Auxílio Creche: 0`)
+                console.log(`Vale Transporte: ${VT}`)
+                console.log(`Vale Alimentação: ${VA}`)
+                console.log(`Desconto INSS: ${salario - INSS}`)
+                console.log(`Desconto FGTS: ${salario - FGTS}`)
+                console.log(`Total a receber: ${salario + bonus + VT + VA - INSS - FGTS}`)
             }
-    
+
+            break;
+        case 3:
+            if (crianca == 'sim') {
+                qntFilhos = Number(prompt(`${nome}, quantos filhos com idade inferior a 7 anos você tem?`))
+                beneficioDep = qntFilhos * 500
+                salarioB = salario + beneficioDep
+
+                bonus = (salario * 1.15) - salario
+
+                console.log(`Nome: ${nome}`)
+                console.log(`Salário Base: ${salario}`)
+                console.log(`Bônus por tempo de serviço: ${bonus}`)
+                console.log(`Auxílio Creche: ${beneficioDep}`)
+                console.log(`Vale Transporte: ${VT}`)
+                console.log(`Vale Alimentação: ${VA}`)
+                console.log(`Desconto INSS: ${salario - INSS}`)
+                console.log(`Desconto FGTS: ${salario - FGTS}`)
+                console.log(`Total a receber: ${salario + bonus + beneficioDep + VT + VA - INSS - FGTS}`)
+
+            } else {
+                console.log(`Nome: ${nome}`)
+                console.log(`Salário Base: ${salario}`)
+                console.log(`Bônus por tempo de serviço: ${bonus}`)
+                console.log(`Auxílio Creche: 0`)
+                console.log(`Vale Transporte: ${VT}`)
+                console.log(`Vale Alimentação: ${VA}`)
+                console.log(`Desconto INSS: ${salario - INSS}`)
+                console.log(`Desconto FGTS: ${salario - FGTS}`)
+                console.log(`Total a receber: ${salario + bonus + VT + VA - INSS - FGTS}`)
+            }
+
+            break;
+        case 4:
+            if (crianca == 'sim') {
+                qntFilhos = Number(prompt(`${nome}, quantos filhos com idade inferior a 7 anos você tem?`))
+                beneficioDep = qntFilhos * 500
+                salarioB = salario + beneficioDep
+
+                bonus = (salario * 1.20) - salario
+
+                console.log(`Nome: ${nome}`)
+                console.log(`Salário Base: ${salario}`)
+                console.log(`Bônus por tempo de serviço: ${bonus}`)
+                console.log(`Auxílio Creche: ${beneficioDep}`)
+                console.log(`Vale Transporte: ${VT}`)
+                console.log(`Vale Alimentação: ${VA}`)
+                console.log(`Desconto INSS: ${salario - INSS}`)
+                console.log(`Desconto FGTS: ${salario - FGTS}`)
+                console.log(`Total a receber: ${salario + bonus + beneficioDep + VT + VA - INSS - FGTS}`)
+
+            } else {
+                console.log(`Nome: ${nome}`)
+                console.log(`Salário Base: ${salario}`)
+                console.log(`Bônus por tempo de serviço: ${bonus}`)
+                console.log(`Auxílio Creche: 0`)
+                console.log(`Vale Transporte: ${VT}`)
+                console.log(`Vale Alimentação: ${VA}`)
+                console.log(`Desconto INSS: ${salario - INSS}`)
+                console.log(`Desconto FGTS: ${salario - FGTS}`)
+                console.log(`Total a receber: ${salario + bonus + VT + VA - INSS - FGTS}`)
+            }
+
+            break;
+
         default:
             console.log('Escolha um valor existente! \n 1 - "menos de 1 ano a 2 anos \n 2 - "2 a 5 anos" \n 3 - "5 a 10 anos" \n 4 - "mais de 10 anos"')
             break;
