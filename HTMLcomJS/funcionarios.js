b5066523627f10735a74b99e9e62bbdc96c1d423
PH <paulofera159@gmail.com>
2024-07-09 10:32:23 -0300

    Funcionarios Feito por PH

--- HTMLcomJS/funcionarios.js
+++ HTMLcomJS/funcionarios.js
@@ -5,23 +5,47 @@
 // Usar o funcionarios.length como medidor do FOR e a cada novo gerado, somar +1 (?++)
 
 
-// funcionarios = []
+funcionarios = []
 
-// let div = document.getElementById('principal')
+let qtd = Number(prompt(`Quantos funcionários serão registrados?`))
 
-// let qtd = Number(prompt(`Quantos funcionários serão registrados?`))
+for (let i = 1; i <= qtd; i++) {
+    let pessoa = {
+        nome: prompt(`Escreva seu nome:`),
+        img: prompt(`Coloque o url do seu link`),
+        idade: prompt(`Escreva sua idade:`),
+        cargo: prompt(`Qual seu cargo?`),
+        salario: Number(prompt(`Escreva seu salário:`))
+    }
 
-// for (let i = 1; i <= qtd; i++) {
-//     let pessoa = {
-//         nome: prompt(`Escreva seu nome:`),
-//         img: prompt(`Escreva o link da sua foto:`),
-//         idade: prompt(`Escreva sua idade:`),
-//         cargo: prompt(`Qual seu cargo?`),
-//         salario: Number(prompt(`Escreva seu salário:`))
-//     }
+    funcionarios.push(pessoa)
+}
 
-//     funcionarios.push(pessoa)
-// }
+funcionarios.forEach(item => {
+    let div = document.getElementById('containerFuncionarios')
+    let divCard = document.createElement('div')
+    divCard.classList.add('cardFuncionario')
+
+    div.appendChild(divCard)
+
+    let h2 = document.createElement('h2')
+    let img = document.createElement('img')
+    let p = document.createElement('p')
+    let h3 = document.createElement('h3')
+    let pp = document.createElement('p')
+
+    divCard.appendChild(h2)
+    divCard.appendChild(img)
+    divCard.appendChild(p)
+    divCard.appendChild(h3)
+    divCard.appendChild(pp)
+
+    h2.innerHTML = `Nome: ${item.nome}`
+    img.src = item.img
+    p.innerHTML = `Idade: ${item.idade}`
+    h3.innerHTML = `Cargo: ${item.cargo}`
+    pp.innerHTML = `Salário: ${item.salario}`
+});
 
 
 
